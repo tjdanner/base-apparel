c92deac898d50102cc8048ac96e4bc88c3108342
Trevor Danner <tjdanner01@gmail.com>
2023-02-07 12:15:24 -0600

    Added client-side email validation, changed some mobile styling with #logo.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const button = document.querySelector('button');
+const input = document.querySelector('input');
+const errorMessage = document.getElementById('error');
+const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 button.onmouseover = e => {
     button.style.boxShadow = '5px 5px 10px lightgray';
     button.onmouseout = e => {
         button.style.boxShadow = '';
+    }
+}
+
+button.onclick = e => {
+    if (input.value.match(re)) {
+        return true
+        error.style.display = '';
+    } else {
+        e.preventDefault();
+        error.style.display = 'block';
     }
 }
